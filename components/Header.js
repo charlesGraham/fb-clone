03aac896b4628f3fbd68bcb381ad5984dea9282e
CHARLES GRAHAM <crg0905@gmail.com>
2021-09-20 23:07:05 -0700

    markup Header component

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,21 @@
+import Image from "next/image";
+
 function Header() {
     return (
         <div>
-            
+            {/* left */}
+            <div>
+                <Image 
+                    src="https://linkspapareactcom/5me"
+                    width={40}
+                    height={40}
+                    layout="fixed"
+                 />
+            </div>
+
+            {/* center */}
+
+            {/* right */}
         </div>
     )
 }
