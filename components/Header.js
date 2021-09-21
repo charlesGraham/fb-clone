eaa25da6c27f6e6baec8921cb89148d1beb9c3fe
CHARLES GRAHAM <crg0905@gmail.com>
2021-09-20 23:24:20 -0700

    style left side of Header

--- components/Header.js
+++ components/Header.js
@@ -17,14 +17,15 @@ import {
 function Header() {
     return (
         <div>
-            <div>
+            <div className="flex items-center">
                 <Image 
                     src="https://links.papareact.com/5me"
                     width={40}
                     height={40}
                     layout="fixed"
                 />
-                <div>
+                <div className="flex">
+                    <SearchIcon className="h-6" />
                     <input type="text" placeholder="Search Facebook" />
                 </div>
             </div>
