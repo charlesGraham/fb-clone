e8e3f6a257ee71f9ed50ddda5660990fdf3f9524
CHARLES GRAHAM <crg0905@gmail.com>
2021-09-21 08:57:15 -0700

    add functionality that pulls session data and passes to render function

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import { getSession } from 'next-auth/client';
 import Head from 'next/head';
 import Header from "../components/Header";
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />;
+  
   return (
     <div>
       <Head>
@@ -17,4 +20,15 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  // Get the user
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session
+    }
+  }
 }
