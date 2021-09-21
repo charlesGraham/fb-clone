be3d7d71aec4f08de8057f585bcd2c98174c44ed
CHARLES GRAHAM <crg0905@gmail.com>
2021-09-21 09:42:43 -0700

    hiting a wall with fb id policy updates

--- components/Login.js
+++ components/Login.js
@@ -10,7 +10,10 @@ function Login() {
                 width={400}
                 objectFit="contain" 
             />
-            <h1 onClick={signIn} className="p-5 bg-blue-500 rounded-full text-white text-center cursor-pointer">Login with Facebook</h1>
+            <h1 
+                onClick={signIn} 
+                className="p-5 bg-blue-500 rounded-full text-white text-center cursor-pointer">Login with Facebook
+            </h1>
         </div>
     )
 }
