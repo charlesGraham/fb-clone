8d5065c5261743ebf344d10f7b917c2651928032
CHARLES GRAHAM <crg0905@gmail.com>
2021-09-20 23:17:12 -0700

    install heroicons

--- components/Header.js
+++ components/Header.js
@@ -3,14 +3,16 @@ import Image from "next/image";
 function Header() {
     return (
         <div>
-            {/* left */}
             <div>
                 <Image 
-                    src="https://linkspapareactcom/5me"
+                    src="https://links.papareact.com/5me"
                     width={40}
                     height={40}
                     layout="fixed"
-                 />
+                />
+                <div>
+                    <input type="text" placeholder="Search Facebook" />
+                </div>
             </div>
 
             {/* center */}
