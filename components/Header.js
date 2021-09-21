4d618a1a49a49b2af079beefb8b288b25aa1cac5
CHARLES GRAHAM <crg0905@gmail.com>
2021-09-20 23:35:17 -0700

    complete styling left side of <Header />

--- components/Header.js
+++ components/Header.js
@@ -24,9 +24,9 @@ function Header() {
                     height={40}
                     layout="fixed"
                 />
-                <div className="flex">
-                    <SearchIcon className="h-6" />
-                    <input type="text" placeholder="Search Facebook" />
+                <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+                    <SearchIcon className="h-6 text-gray-600" />
+                    <input className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search Facebook" />
                 </div>
             </div>
 
